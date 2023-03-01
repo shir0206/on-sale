6467e2dca1790cf88d89c9bcd468f2e70eeffbe1
Shir Zabolotny <40990488+shir0206@users.noreply.github.com>
2023-03-01 12:50:46 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="clock">
+      <div className="circle">
+        <div className="ring">
+          <div className="ball-background"></div>
+          <div className="ball"></div>
+        </div>
+      </div>
+      <div className="text">ON SALE!</div>
+    </div>
+  );
+}
